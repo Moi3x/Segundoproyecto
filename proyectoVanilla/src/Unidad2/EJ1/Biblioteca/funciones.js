be704c5d297d9ffe565b359c91a39dfcbb06b712
Moi3x <mgarciag4@alu.cipfpvalledeelda.com>
2026-09-23 20:01:35 +0200

    Ejercicios medio terminados

--- proyectoVanilla/src/Unidad2/EJ1/Biblioteca/funciones.js
+++ proyectoVanilla/src/Unidad2/EJ1/Biblioteca/funciones.js
@@ -12,16 +12,89 @@ return suma/arguments.length;
 
 }
 function equipoMayor(){
-const mediaMasAlta=0;
-const 
+let mediaMasAlta=0;
+let mejorEquipo="";
+let empate = false;
+
 for (let i = 0; i < arguments.length; i++){
+   const nombre = arguments[i];
+   const media =arguments[i+1];
+if(media > mediaMasAlta){
+    mediaMasAlta=media;
+    mejorEquipo=nombre;
+    empate=false;
+}else if (media ===mediaMasAlta){
+    empate=true;
+}
+}
+return {mejorEquipo,mediaMasAlta,empate};
+
+}
+function multiplicarNumero(num1 ,num2){
+    let comodin=num2;
+    let cadena="";
+
+for(let i = 1;i <= num1;i++){
+    cadena +=comodin + " ";
     
+    comodin = comodin*2;
 }
+return cadena.trim();
 }
 
+//parametros por defecto
+//nombreProducto=Producto generico", precio=100,impuesto=21
+
+function impuestoProducto(nombreProducto,precio,impuesto){
+nombreProducto=(nombreProducto===""||nombreProducto===undefined)?"Producto generico": nombreProducto;
+precio=(precio===undefined||precio==null)? 100:precio;
+impuesto=(impuesto===undefined||impuesto==null)? 21:impuesto;
+if(isNaN(precio)||isNaN(impuesto)){
+    console.error("Los numeros introducidos en el precio o el impuesto no son validos");
+return;
+}
+const precioFinal= precio +(precio*impuesto/100);
+
+return `${nombreProducto} Precio: ${precio} Iva: ${precioFinal}`;
+}
+
+function numeroMeses(numero){
+    if (numero===null ||typeof numero !=="number" ||Number.isNaN(numero)||!Number.isInteger(numero)||numero<=0 ||numero>12){
+        console.error("El numero introducido esta fuera de rango o es incorrecto");
+    return;
+    }
+   const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+    return meses[numero-1];
+
+  /*  switch(numero){
+        case 1:
+            return "Enero";
+        case 2:
+           return  "Febrero";
+        case 3:
+            return "marzo";
+        case 4:
+            return "abril";
+        case 5:
+            return "mayo";
+        case 6:
+            return "junio" ;
+        case 7:
+            return "julio";
+        case 8:
+            return "agosto";
+        case 9:
+            return "septiembre";
+        case 10:
+            return "octubre";
+        case 11: 
+            return "noviembre";
+        case 12:
+            return "diciembre";
+        }*/
 }
 
 
+export {calcularIMC , mediaEquipo , equipoMayor,multiplicarNumero,impuestoProducto,numeroMeses};
 
-
-export {calcularIMC , mediaEquipo};
--- proyectoVanilla/src/Unidad2/EJ1/main.js
+++ proyectoVanilla/src/Unidad2/EJ1/main.js
@@ -1,12 +1,33 @@
-import { calcularIMC, mediaEquipo, resultado } from "./Biblioteca/funciones.js";
+import { calcularIMC, mediaEquipo ,equipoMayor,multiplicarNumero,impuestoProducto,numeroMeses} from "./Biblioteca/funciones.js";
+
+// EJERCICIO 1
+
 console.log("Masa Marcos");
-var marcos = calcularIMC(70,160);
+const marcos = calcularIMC(70,160);
 console.log(marcos);
 console.log("Masa Juan");
-var juan = calcularIMC(60, 160);
+const juan = calcularIMC(60, 160);
 console.log(juan);
 console.log(`Tiene Marcos un IMC mayor a Juan: ${marcos > juan}`);
-var juanEquipo =mediaEquipo(89,120,103);
-var miguelEquipo=mediaEquipo(116,94,123);
-var MariaEquipo=mediaEquipo(97,134,105);
-var mayorMedia =Equipo
+
+// EJERCICIO 2
+
+const juanEquipo =mediaEquipo(89,120,103);
+const miguelEquipo=mediaEquipo(116,94,123);
+const MariaEquipo=mediaEquipo(97,134,105);
+
+const r1 =equipoMayor("Juan",juanEquipo,"Miguel",miguelEquipo,"Maria",MariaEquipo);
+
+if (r1.empate) {
+    console.log(`Hay un empate con una media de ${r1.mediaMasAlta}`);
+} else {
+    console.log(`El equipo de ${r1.mejorEquipo} tiene la media más alta: ${r1.mediaMasAlta}`);
+}
+// Ejercicio 3
+console.log(multiplicarNumero(4,7));
+
+//Ejercicio 4
+
+console.log(impuestoProducto());
+
+console.log(numeroMeses(12));
